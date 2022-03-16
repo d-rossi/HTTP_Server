3a66ae9a068fbb12d771a4032f4fe86250750980
d-rossi <david.rossi101@gmail.com>
2022-03-15 21:28:50 -0400

    handle client request

--- httpfs.py
+++ httpfs.py
@@ -23,6 +23,86 @@ def start_server(verbose, port, dir):
 		conn, addr = server.accept()
 		threading.Thread(target = handle_client_request,args=(conn, addr, port, dir, verbose)).start()
 
+def handle_client_request(conn, addr, port, dir, verbose):
+	print(f"[NEW CONNECTION] {addr} connected.")
+	verbose_msg = ""
+	try:
+		data = conn.recv(1024)
+		data = data.decode('utf-8')
+
+		# Parse the request from client 
+		request_content = data.split("\r\n\r\n")
+		header_contents = request_content[0].split()
+		request_method = header_contents[0]
+		verbose_msg += f"REQUEST METHOD: {request_method}\n"
+		request_path = header_contents[1].split(f"{port}/")[1]
+		if not request_path:
+			request_path = "/"
+		verbose_msg += f"REQUEST PATH: {request_path}\n"
+		
+		request_body = None
+		if len(request_content)>1: 
+			request_body = request_content[1]
+			verbose_msg += f"REQUEST BODY: {request_body}\n"
+		
+		###################### Do GET or POST ######################
+		response_body = ""
+		status_line = "HTTP/1.0 "
+		files = os.listdir(dir)
+		verbose_msg += f"CONTENT OF DIRECTORY: {files}\n"
+
+		#bad request if client tries to go to parent directory
+		if ".." in request_path:
+			status_line += "403 Forbidden \r\n"
+			verbose_msg += "[ERROR 403] Forbidden access outside of working directory\n"
+
+		elif request_method == "GET":
+			if request_path == "/":
+				for file in files:
+					response_body += file + "\n"
+				status_line += "200 OK \r\n"
+				verbose_msg += "[SUCCESS] Files in working directory were retrieved\n"
+			else:
+				try:
+					with open(dir+"/"+request_path, "r") as file:
+						file_content = file.read()
+					response_body = file_content
+					status_line += "200 OK \r\n"
+					verbose_msg += "[SUCCESS] File content was retrieved\n"
+				except EnvironmentError:
+					status_line += "404 Not Found \r\n"
+				if not response_body:
+					verbose_msg += "[ERROR 404] File not found in working directory\n"
+
+		elif request_method == "POST" and request_body != None:
+			try:
+				with open(dir+"/"+request_path, "w") as file:
+					file.write(request_body)
+				status_line += "200 OK \r\n"
+				verbose_msg += "[SUCCESS] File content was updated\n"
+			except EnvironmentError:
+				status_line += "404 Not Found \r\n"
+				verbose_msg += "[ERROR 404] File not found in working directory\n"
+		else:
+			status_line += "400 Bad Request \r\n"
+			verbose_msg += "[ERROR 400] Bad Request\n"
+		
+		# Send Response Message back to client
+		response_headers = "Date: " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " GMT\r\n"
+		response_headers += "Connection: close\r\n"
+
+		response_headers += "Server: httpfs \r\nAccept-Ranges: bytes \r\n"
+		body_length = len(response_body)
+
+		content_headers = "Content-Type: text \r\nContent-Length: " + str(body_length) + "\r\n\r\n"
+		response = status_line + response_headers + content_headers + response_body
+
+		conn.sendall(response.encode('utf-8'))
+		if verbose:
+			print(verbose_msg)
+	finally:
+		print(f"[CLOSE CONNECTION] {addr} closed.\n")
+		conn.close()
 
 
 parser = argparse.ArgumentParser(description="httpfs is a simple file server.", usage="httpfs [-v] [-p PORT] [-d PATH-TO-DIR]")
